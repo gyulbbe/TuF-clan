7fad3e2f844d46203d90789b6b6a5fd0d88f1a98
한결 <gyulbbe@gmail.com>
2025-11-02 00:21:00 +0900

    first commit

---
+++ src/test/java/io/github/gyulbbe/TufApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gyulbbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TufApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
