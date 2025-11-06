29a06bdffbd2921572a8362552baa424cf0629ef
한결 <gyulbbe@gmail.com>
2025-11-06 22:52:46 +0900

    feat : SPEECH_STYLE_LEARNING -> VECTORS insert 배치

--- src/main/java/io/github/gyulbbe/speechLearning/controller/SpeechLearningController.java
+++ src/main/java/io/github/gyulbbe/speechLearning/controller/SpeechLearningController.java
@@ -1,6 +1,7 @@
 package io.github.gyulbbe.speechLearning.controller;
 
 import io.github.gyulbbe.common.dto.ResponseDto;
+import io.github.gyulbbe.speechLearning.dto.SpeechLearningDto;
 import io.github.gyulbbe.speechLearning.dto.insertSpeechLearningDto;
 import io.github.gyulbbe.speechLearning.service.SpeechLearningService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequestMapping("/speech-learning")
 @RequiredArgsConstructor
@@ -16,9 +18,19 @@ public class SpeechLearningController {
 
     private final SpeechLearningService speechLearningService;
 
+    @GetMapping("/all")
+    public ResponseEntity<ResponseDto<List<SpeechLearningDto>>> findAllSpeechLearning() {
+        return ResponseEntity.ok(speechLearningService.findAllSpeechLearning());
+    }
+
     @PostMapping("/insert")
     public ResponseEntity<ResponseDto<Void>> insertSpeechLearning(@Valid @RequestBody insertSpeechLearningDto insertSpeechLearningDto) {
         return ResponseEntity.ok(speechLearningService.insertSpeechLearning(insertSpeechLearningDto));
+    }
+
+    @PostMapping("/embed-all")
+    public ResponseEntity<ResponseDto<String>> embedAllSpeechLearning() {
+        return ResponseEntity.ok(speechLearningService.embedAllSpeechLearning());
     }
 
     @GetMapping("/test")
