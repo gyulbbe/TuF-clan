2ab7e0c4edd153df0e6af3f120effe457d7e5f71
한결 <gyulbbe@gmail.com>
2025-11-08 18:14:59 +0900

    chore : cors 오픈

---
+++ src/main/java/io/github/gyulbbe/config/WebConfig.java
@@ -0,0 +1,17 @@
+package io.github.gyulbbe.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://tufclan.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
